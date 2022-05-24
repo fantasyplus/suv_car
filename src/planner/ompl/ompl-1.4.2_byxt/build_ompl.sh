552c52c84ecfb865b63aa53a95dd88f18f81648c
ros <530925206@qq.com>
2022-05-24 10:31:47 +0800

    change build_ompl.sh

--- src/planner/ompl/ompl-1.4.2_byxt/build_ompl.sh
+++ src/planner/ompl/ompl-1.4.2_byxt/build_ompl.sh
@@ -1,6 +1,8 @@
     #!/bin/bash
 
-    cd ompl-1.4.2/build/Release
+    cd ompl-1.4.2
+    mkdir -p build/Release
+    cd build/Release
     cmake ../.. -DPYTHON_EXEC=/usr/bin/python${PYTHONV}
     make
     sudo make install
